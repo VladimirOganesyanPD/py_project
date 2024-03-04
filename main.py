525086604cd75f3a44bffa7875c4e58446c364e3
Vladimir Oganesyan <mr.lalochkina@mail.ru>
2024-03-04 21:45:53 +0300

    Added function for addition Added function for addition second time Add main.py with function call Add main.py with function call second time

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from funcs import add
+from funcs import subtract
 
 if __name__ == "__main__":
     result = add(10, 5)
     print("Result of addition:", result)
+    result = subtract(10, 5)
+    print("Result of subtraction:", result)
